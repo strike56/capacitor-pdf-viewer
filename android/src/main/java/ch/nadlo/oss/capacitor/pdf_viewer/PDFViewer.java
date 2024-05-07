ca1a89112ab3d5c2a833b5151232f58f93d9431f
Leonid Marchenko <dev.leonid.marchenko@gmail.com>
2024-05-07 18:04:19 +0300

    chore(v0.1.2): fix PDF Viewer close method

--- android/src/main/java/ch/nadlo/oss/capacitor/pdf_viewer/PDFViewer.java
+++ android/src/main/java/ch/nadlo/oss/capacitor/pdf_viewer/PDFViewer.java
@@ -3,7 +3,6 @@ package ch.nadlo.oss.capacitor.pdf_viewer;
 import android.app.Activity;
 import android.app.Application;
 import android.content.Intent;
-import android.util.Log;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -16,7 +15,6 @@ import java.util.List;
 
 public class PDFViewer {
     private Bridge bridge = null;
-    private Intent activeIntent = null;
 
     private static final List<Activity> activeActivities = new ArrayList<>();
 
@@ -29,12 +27,16 @@ public class PDFViewer {
             app.registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
                 @Override
                 public void onActivityCreated(@NonNull Activity activity, Bundle savedInstanceState) {
-                    activeActivities.add(activity);
+                    if (activity.toString().startsWith("com.rajat.pdfviewer.PdfViewerActivity")) {
+                        activeActivities.add(activity);
+                    }
                 }
 
                 @Override
                 public void onActivityDestroyed(@NonNull Activity activity) {
-                    activeActivities.remove(activity);
+                    if (activity.toString().startsWith("com.rajat.pdfviewer.PdfViewerActivity")) {
+                        activeActivities.remove(activity);
+                    }
                 }
 
                 // Other lifecycle methods can remain empty
@@ -57,8 +59,10 @@ public class PDFViewer {
     }
 
     public void openViewer(String url, String title) {
+        this.close();
+
         // Create an intent with unique extras or identifiers
-        activeIntent = PdfViewerActivity.Companion.launchPdfFromUrl(
+        Intent activeIntent = PdfViewerActivity.Companion.launchPdfFromUrl(
                 this.bridge.getContext(),
                 url,
                 title,
@@ -66,36 +70,12 @@ public class PDFViewer {
                 false
         );
 
-        activeIntent.putExtra("UNIQUE_ID", "PDF_VIEWER_1"); // Or some other unique identifier
-
         this.bridge.getActivity().startActivity(activeIntent);
     }
 
     public void close() {
-        Log.v("TriAppPDFViewer", "Closing all PDF Viewer activities.");
-
-        if (activeIntent != null) {
-            Activity pdfViewerActivity = getActivityByIntent(activeIntent);
-            if (pdfViewerActivity != null) {
-                pdfViewerActivity.finish(); // Explicitly close the PDF viewer activity
-            } else {
-                Log.v("TriAppPDFViewer", "No PDF Viewer activity found in stack.");
-            }
-
-            activeIntent = null;
-        }
-    }
-
-    private Activity getActivityByIntent(Intent intent) {
         for (Activity activity : activeActivities) {
-            // Match activities by comparing specific extras or tags
-            String activityId = activity.getIntent().getStringExtra("UNIQUE_ID");
-            String intentId = intent.getStringExtra("UNIQUE_ID");
-
-            if (activityId != null && activityId.equals(intentId)) {
-                return activity; // Match found
-            }
+            activity.finish();
         }
-        return null; // No match found
     }
 }
